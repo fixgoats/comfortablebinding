7eb5251a6098da45ec19425821168b303a040690
fixgoats <fixgoats@mGLaDOS>
2025-09-23 17:54:19 +0000

    try to add dos

--- src/main.cpp
+++ src/main.cpp
@@ -189,25 +189,6 @@ std::vector<Point> readPoints(const std::string& fname) {
  * alla kjarnana á tölvunni nema notandinn biðji um það.
  */
 
-void pointsToPeriodicCouplings(const MatrixX2d& points, f64 rsq,
-                               std::optional<double> lx,
-                               std::optional<double> ly) {
-  /* This function is meant to create couplings for approximants of
-   * quasicrystals. The unit cell vectors are taken to be parallel to the x and
-   * y axes. If lx and/or ly have values, those are taken to be the x/y lengths
-   * of the unit cell, otherwise the lengths are estimated to be the x/y extents
-   * of the approximant plus the average x/y separation between neighbouring
-   * points (actually that sounds rather complicated, I'd have to iterate over
-   * the whole lattice, finding the neighbours, and then... actually that might
-   * be fine, if I do one pass for the approximant and then somehow handle edge
-   * cases... or! I could use rsq!) So, if I turn one unit cell into four, I'll
-   * have to check which cell the neighbour is in and correlate the neighbour
-   * with an "atom" in the original cell. So, I might filter out the points that
-   * are less than r from the x-axis, the y-axis and their intersection and only
-   * add those to the extended grid. Make a k-d tree?
-   */
-}
-
 template <class D>
 void saveEigen(const std::string& fname, const Eigen::MatrixBase<D>& x) {
   std::ofstream f(fname);
@@ -311,24 +292,6 @@ void update_hamiltonian(MatrixXcd& H, const std::vector<Neighbour>& nbs,
   }
 }
 
-template <class T>
-struct Arr3D : std::vector<T> {
-  size_t x;
-  size_t y;
-  size_t z;
-
-  Arr3D(size_t x, size_t y, size_t z)
-      : std::vector<T>(x * y * z), x{x}, y{y}, z{z} {}
-
-  T& operator()(size_t i, size_t j, size_t k) {
-    return (*this)[y * z * i + z * j + k];
-  }
-
-  T operator()(size_t i, size_t j, size_t k) const {
-    return (*this)[y * z * i + z * j + k];
-  }
-};
-
 template <class PointT>
 double avgNNDist(kdt::KDTree<PointT>& kdtree,
                  const std::vector<PointT>& points) {
@@ -354,6 +317,127 @@ void standardise(std::vector<Point>& points) {
     p[0] -= minx;
     p[1] -= miny;
   });
+}
+
+std::vector<Point> extended_grid(const std::vector<Point>& base,
+                                 const std::vector<int>& x_edge,
+                                 const std::vector<int>& y_edge,
+                                 const std::vector<int>& corner, double Lx,
+                                 double Ly) {
+  std::vector<Point> final_grid(base.size() + x_edge.size() + y_edge.size() +
+                                corner.size());
+  std::copy(base.cbegin(), base.cend(), final_grid.begin());
+  size_t offset = base.size();
+  for (size_t i = 0; i < x_edge.size(); i++) {
+    Point p = base[x_edge[i]];
+    p[1] += Ly;
+    final_grid[offset + i] = p;
+  }
+  offset += x_edge.size();
+  for (size_t i = 0; i < y_edge.size(); i++) {
+    Point p = base[y_edge[i]];
+    p[0] += Lx;
+    final_grid[offset + i] = p;
+  }
+  offset += y_edge.size();
+  for (size_t i = 0; i < corner.size(); i++) {
+    Point p = base[corner[i]];
+    p[0] += Lx;
+    p[1] += Ly;
+    final_grid[offset + i] = p;
+  }
+  return final_grid;
+}
+
+void pointsToPeriodicCouplings(std::vector<Point>& points, f64 rsq,
+                               std::optional<double> lx,
+                               std::optional<double> ly) {
+  standardise(points);
+  kdt::KDTree<Point> kdtree(points);
+  double maxx = points[kdtree.axisFindMax(0)][0];
+  double maxy = points[kdtree.axisFindMax(1)][1];
+  kdtree.build(points);
+  double avg_nn_dist = 1.0;
+  if (points.size() > 1) {
+    avg_nn_dist = avgNNDist(kdtree, points);
+  }
+  double Lx = maxx + avg_nn_dist;
+  double Ly = maxy + avg_nn_dist;
+  std::cout << std::format("Lx is {}\n"
+                           "Ly is {}\n"
+                           "Average distance to next neighbour is: {}\n",
+                           Lx, Ly, avg_nn_dist);
+  double search_radius = 2.01 * avg_nn_dist;
+  auto x_edge = kdtree.axisSearch(0, search_radius - avg_nn_dist + 1e-6);
+  auto y_edge = kdtree.axisSearch(1, search_radius - avg_nn_dist + 1e-6);
+  std::vector<int> xy_corner;
+  // Estimate of how many points there are in the intersection of the edges.
+  xy_corner.reserve((size_t)((double)(x_edge.size() * y_edge.size()) /
+                             (double)points.size()));
+  for (const auto xidx : x_edge) {
+    for (const auto yidx : y_edge) {
+      if (xidx == yidx) {
+        xy_corner.push_back(xidx);
+      }
+    }
+  }
+  auto final_grid = extended_grid(points, x_edge, y_edge, xy_corner, Lx, Ly);
+  kdtree.build(final_grid);
+  std::vector<Neighbour> nb_info;
+  for (size_t i = 0; i < points.size(); i++) {
+    auto q = final_grid[i];
+    auto nbs = kdtree.radiusSearch(q, search_radius);
+    for (const auto idx : nbs) {
+      if ((size_t)idx > i) {
+        auto p = final_grid[idx];
+        Vector2d d = {p[0] - q[0], p[1] - q[1]};
+        nb_info.emplace_back(i, p.idx, d);
+      }
+    }
+  }
+  MatrixXcd hamiltonian = MatrixXcd::Zero(points.size(), points.size());
+  constexpr u32 ksamples = 20;
+  Vector2d dual1{2 * M_PI / Lx, 0};
+  Vector2d dual2{0, 2 * M_PI / Ly};
+  std::vector<double> energies(ksamples * ksamples * points.size());
+  auto energy_view =
+      std::mdspan(energies.data(), ksamples, ksamples, points.size());
+  for (u32 j = 0; j < ksamples; j++) {
+    double yfrac = (double)j / ksamples;
+    for (u32 i = 0; i < ksamples; i++) {
+      double xfrac = (double)i / ksamples;
+      update_hamiltonian(
+          hamiltonian, nb_info, xfrac * dual1 + yfrac * dual2,
+          [](Vector2d) { return c64{-1, 0}; }, i | j);
+      SelfAdjointEigenSolver<MatrixXcd> es;
+      es.compute(hamiltonian);
+      for (size_t k = 0; k < points.size(); k++) {
+        energy_view[i, j, k] = es.eigenvalues()[k];
+        // std::cout << energies(i, j, k) << '\n';
+      }
+    }
+  }
+  hsize_t dims[3] = {ksamples, ksamples, points.size()};
+  H5::DataSpace space(3, dims);
+  H5::H5File file("energies.h5", H5F_ACC_TRUNC);
+  H5::DataSet dataset(
+      file.createDataSet("aaa", H5::PredType::NATIVE_DOUBLE, space));
+  dataset.write(energies.data(), H5::PredType::NATIVE_DOUBLE);
+
+  /* This function is meant to create couplings for approximants of
+   * quasicrystals. The unit cell vectors are taken to be parallel to the x and
+   * y axes. If lx and/or ly have values, those are taken to be the x/y lengths
+   * of the unit cell, otherwise the lengths are estimated to be the x/y extents
+   * of the approximant plus the average x/y separation between neighbouring
+   * points (actually that sounds rather complicated, I'd have to iterate over
+   * the whole lattice, finding the neighbours, and then... actually that might
+   * be fine, if I do one pass for the approximant and then somehow handle edge
+   * cases... or! I could use rsq!) So, if I turn one unit cell into four, I'll
+   * have to check which cell the neighbour is in and correlate the neighbour
+   * with an "atom" in the original cell. So, I might filter out the points that
+   * are less than r from the x-axis, the y-axis and their intersection and only
+   * add those to the extended grid. Make a k-d tree?
+   */
 }
 
 int main(const int argc, const char* const* argv) {
@@ -446,7 +530,9 @@ int main(const int argc, const char* const* argv) {
     constexpr u32 ksamples = 20;
     Vector2d dual1{2 * M_PI / Lx, 0};
     Vector2d dual2{0, 2 * M_PI / Ly};
-    Arr3D<double> energies(ksamples, ksamples, vec.size());
+    std::vector<double> energies(ksamples * ksamples * vec.size());
+    auto energy_view =
+        std::mdspan(energies.data(), ksamples, ksamples, vec.size());
     for (u32 j = 0; j < ksamples; j++) {
       double yfrac = (double)j / ksamples;
       for (u32 i = 0; i < ksamples; i++) {
@@ -457,7 +543,7 @@ int main(const int argc, const char* const* argv) {
         SelfAdjointEigenSolver<MatrixXcd> es;
         es.compute(hamiltonian);
         for (size_t k = 0; k < vec.size(); k++) {
-          energies(i, j, k) = es.eigenvalues()[k];
+          energy_view[i, j, k] = es.eigenvalues()[k];
           // std::cout << energies(i, j, k) << '\n';
         }
       }
